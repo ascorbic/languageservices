10dae662cace4d92e57267f39d4275a0afa40334
Matt Kane <m@mk.gg>
2024-09-26 16:37:12 +0100

    ship dist lol

---
+++ expressions/dist/index.js
@@ -0,0 +1,10 @@
+export { Expr } from "./ast.js";
+export { complete } from "./completion.js";
+export { DescriptionDictionary, isDescriptionDictionary } from "./completion/descriptionDictionary.js";
+export * as data from "./data/index.js";
+export { ExpressionError, ExpressionEvaluationError } from "./errors.js";
+export { Evaluator } from "./evaluator.js";
+export { wellKnownFunctions } from "./funcs.js";
+export { Lexer } from "./lexer.js";
+export { Parser } from "./parser.js";
+//# sourceMappingURL=index.js.map
